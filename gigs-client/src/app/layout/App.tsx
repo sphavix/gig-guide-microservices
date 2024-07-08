98308b32a8546d48d4cf27e02bafa2b309c9a605
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-07-08 19:55:54 +0200

    Implemented the React Types or Interfaces

--- gigs-client/src/app/layout/App.tsx
+++ gigs-client/src/app/layout/App.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Gig } from '../models/gig';
 
 function App() {
-  const [gigs, setGigs] = useState([]);
+  const [gigs, setGigs] = useState<Gig[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5001/api/gigs')
+    axios.get<Gig[]>('http://localhost:5001/api/gigs/getallgigs')
         .then(respons => {
           setGigs(respons.data)
         })
@@ -18,7 +18,7 @@ function App() {
       <Header as='h2' icon='users' content='Local Gigs' />
       <List>
         {
-          gigs.map((gig: any) => (
+          gigs.map((gig) => (
             <List.Item key={gig.id}>
               {gig.title}
             </List.Item>
